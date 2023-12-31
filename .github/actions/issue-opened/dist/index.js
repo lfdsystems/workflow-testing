c5f519945e26e6ef074a560e3198a750c0bf4d28
Vipin Kumar <leovipin1@gmail.com>
2023-12-31 13:17:55 +0530

    issue-opened-get-labels-list

--- .github/actions/issue-opened/dist/index.js
+++ .github/actions/issue-opened/dist/index.js
@@ -28486,12 +28486,21 @@ async function run() {
     const payload = github.context.payload
 
     const sender = payload.sender.login
+    const owner = payload.repository.owner.login
+    const repo = payload.repository.name
+    const issue_number = payload.issue.number
 
     const token = core.getInput('github_token', { required: true })
 
     const octokit = new github.getOctokit(token)
 
-    console.log(sender)
+    const labels = await octokit.rest.issues.listLabelsOnIssue({
+      owner,
+      repo,
+      issue_number
+    })
+
+    console.log(labels)
   } catch (error) {
     // Fail the workflow step if an error occurs
     core.setFailed(error.message)
