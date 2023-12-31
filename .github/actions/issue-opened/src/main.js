a37ee3234256f13873e2162165eae8a2e1762559
Vipin Kumar <leovipin1@gmail.com>
2023-12-31 09:12:20 +0530

    remove-test-action

--- .github/actions/issue-opened/src/main.js
+++ .github/actions/issue-opened/src/main.js
@@ -7,18 +7,7 @@ const github = require('@actions/github')
  */
 async function run() {
   try {
-    // The `who-to-greet` input is defined in action metadata file
-    const whoToGreet = core.getInput('who-to-greet', { required: true })
-    core.info(`Hello, ${whoToGreet}!`)
-
-    // Get the current time and set as an output
-    const time = new Date().toTimeString()
-    core.setOutput('time', time)
-
-    // Output the payload for debugging
-    core.info(
-      `The event payload: ${JSON.stringify(github.context.payload, null, 2)}`
-    )
+    
   } catch (error) {
     // Fail the workflow step if an error occurs
     core.setFailed(error.message)
